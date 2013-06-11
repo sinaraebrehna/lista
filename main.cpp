80c0a9d371c81544a38551e0c4196d55f87845e6
sinarapg <sinarapg@Sinara>
2013-06-11 09:49:19 -0300

    commit lista

---
+++ main.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+
+using namespace std;
+
+
+class no
+{
+private:
+public:
+    int info;
+    no* prox;
+};
+
+no*concatenalistacs(no* lcs1, no* lcs2)
+{
+    no*aux1,*aux2;
+    aux1=lcs1->prox;
+    aux2=lcs2->prox;
+    lcs1->prox=aux2;
+    return lcs2;
+}
+
+
+int main()
+{
+    no*lcs2;
+    lcs2->info=(2*4);
+    cout<<lcs2->info<<endl;
+    return 0;
+}
